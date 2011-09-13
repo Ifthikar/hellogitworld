8ea0f09bca83d272daa6f6e4b7e13da6eeaa5f1e
Ruben Barroso <ruben@txvia.com>
2011-09-13 21:32:03 +0200

    Greeting in Spanish

--- src/Main.groovy
+++ src/Main.groovy
@@ -6,7 +6,7 @@ import static Sum.sum
 def name = "Manfred"
 int programmingPoints = 9000
 
-println "Hello ${name}"
+println "Hello ${name}. Hola in Spanish"
 println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${square(programmingPoints)}"
 println "${programmingPoints} divided by 2 bonus points is ${divide(programmingPoints, 2)}"
